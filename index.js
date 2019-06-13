71b297c6b8a55890a3905376d0bc0b003ad6c3a9
fardeen15 <fardeenmemon17@gmail.com>
2019-06-13 08:28:20 -0700

    quantity complete

--- index.js
+++ index.js
@@ -2,118 +2,147 @@ var obj = [
     {
         src: "./images/watch1.jpg",
         name: "rado",
-        id : 0,
+        quantity: 0,
+        id: 0,
         price: 500
     },
     {
         src: "./images/watch1.jpg",
         name: "rado",
-        id : 1,
+        id: 1,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch2.jpg",
         name: "rado",
-        id : 2,
+        id: 2,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch3.jpg",
         name: "rado",
-        id : 3,
+        id: 3,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch4.jpg",
         name: "rado",
-        id : 4,
+        id: 4,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch5.jpg",
         name: "rado",
-        id : 5,
+        id: 5,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch6.jpg",
         name: "rado",
-        id : 6,
+        id: 6,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch7.jpg",
-        id : 7,
+        id: 7,
         name: "rado",
-        price: 500
+        price: 500,
+        quantity: 0,
     },
     {
         src: "./images/watch8.jpg",
         name: "rado",
-        id : 8,
+        id: 8,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch9.jpg",
         name: "rado",
-        id : 9,
+        id: 9,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch10.jpg",
         name: "rado",
-        id : 10,
+        id: 10,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch11.jpg",
         name: "rado",
-        id : 11,
+        id: 11,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch12.jpg",
         name: "rado",
-        id : 12,
+        id: 12,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch13.jpg",
         name: "rado",
-        id : 13,
+        id: 13,
+        quantity: 0,
         price: 500
     },
     {
         src: "./images/watch14.jpg",
         name: "rado",
-        id : 14,
+        id: 14,
+        quantity: 0,
         price: 500
     }
 ]
-
+// var obj2 = JSON.parse(localStorage.getItem("data"));
 var div = document.querySelector("#bodyDiv");
 for (var i = 0; i < obj.length; i++) {
     div.innerHTML +=
         `<div class = "cardDiv" id = "${i}">
         <img src = "${obj[i].src}"></img>
-        <p>${obj[i].name}<br><br> ${obj[i].price}<br><br><span class= "icon" onclick = "remove(${i})">-</span> <span class = "icon" onclick = "add(${i})">+</span></p>
+        <p>${obj[i].name}<br><br> ${obj[i].price}<br><br> <span id = "quantity${i}"></span><ber><br> <span class= "icon" onclick = "remove(${i})">-</span> <span class = "icon" onclick = "add(${i})">+</span></p>
     </div>`
 }
-function add(i) {
-    // console.log(obj[i])
-
-    var obj2 = JSON.parse(localStorage.getItem("data"));
-    // var obj2 = [get]
+var obj2;
+function add(index) {
+    obj2 = JSON.parse(localStorage.getItem("data"));
     if (obj2 === null) {
         obj2 = [];
     }
-    obj[i].id1 = i;
-    obj2.push(obj[i]);
-    console.log(obj2)
-    localStorage.setItem("data", JSON.stringify(obj2));
-
+    obj2.push(obj[index]);
+    for (var i = 0; i < obj2.length; i++) {
+        if (obj[index].id === obj2[i].id) {
+            obj2[i].quantity = ++obj2[i].quantity;
+            if (obj[index].src === obj2[i].src) {
+                localStorage.setItem("data", JSON.stringify(obj2));
+            }
+        }
+    }
     counter()
 }
+// var quantity1 = document.querySelector(`#quantity${index}`)
+// for(var i= 0; i < obj2.length; i++){
+//         // if(obj[i].id === obj2[i].id){
+//                 quantity1.innerHTML = `(Quantity)${obj2[i].quantity}`;
+//                 // console.log("correct");
+//             // }
+//         }
+//         for(var i = 0 ; i < obj2.length; i++){
+
+//             }
+
+
 
 var counter1 = document.querySelector(".counter");
 var get = localStorage.getItem("counter");
@@ -126,44 +155,39 @@ if (get === null) {
 console.log(get)
 
 
+
 function counter() {
     var counter1 = document.querySelector(".counter");
     var counter2 = document.querySelector(".counter").innerHTML;
     var plus = ++counter2;
     counter1.innerHTML = plus
 
-    console.log(plus)
+
 
 
     localStorage.setItem("counter", plus);
 }
 
+
+
+
+
+
 function remove(i) {
     var counter3 = document.querySelector(".counter");
     var counter4 = document.querySelector(".counter").innerHTML;
     var less = --counter4;
-    if(less >= 0){
+    if (less >= 0) {
         counter3.innerHTML = less
     }
     console.log(less)
 
-    if(less >= 0){
+    if (less >= 0) {
         localStorage.setItem("counter", less);
     }
-    removeobj(i)
-    
+
 }
 
-function removeobj(i){
-    // console.log(i);
-    var data1 = JSON.parse(localStorage.getItem("data"));
-    // if(i === obj[i].id){
-        data1.splice(obj[i].id,1);
-        console.log(data1);
-        localStorage.setItem("data", JSON.stringify(data1)) 
-    // }
-    
-}
 function pageChng1() {
     window.location.href = "index.html"
 }
